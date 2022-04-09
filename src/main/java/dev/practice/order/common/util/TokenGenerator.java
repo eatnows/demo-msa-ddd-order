a023231d6ec1ac5d71b24544a7b7b6a5feb85cb9
eatnows <gagyasindev@gmail.com>
2022-04-09 17:18:23 +0900

    feat: item entity 연관관계 설정

--- src/main/java/dev/practice/order/common/util/TokenGenerator.java
+++ src/main/java/dev/practice/order/common/util/TokenGenerator.java
@@ -2,10 +2,13 @@ package dev.practice.order.common.util;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.util.UUID;
+
 public class TokenGenerator {
     private static final int TOKEN_LENGTH = 20;
 
     public static String randomCharacter(int length) {
+        
         return RandomStringUtils.randomAlphanumeric(length);
     }
 
